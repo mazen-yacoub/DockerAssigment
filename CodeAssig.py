2a3c40a43f0f1bdc6f6b6f0694e2dd3f51ddeb86
mazen-yacoub <130481212+mazen-yacoub@users.noreply.github.com>
2024-04-22 18:39:05 +0200

    python script .py file

---
+++ CodeAssig.py
@@ -0,0 +1,47 @@
+
+import re
+from collections import Counter
+
+# Path to the text file
+path = "C:\\Users\\myacoubalex\\Desktop\\random_paragraphs.txt"
+
+# Read the text file
+with open(path, "r") as file:
+    text = file.read()
+
+# Define stop words
+StopWords = [
+    "a", "an", "the", "of", "is", "in", "on", "to", "for", "as", "with", "by",
+    "at", "which", "be", "have", "it", "that", "this", "are", "from", "or",
+    "your", "our", "those", "such", "too", "my", "ones", "would", "them", "and"
+]
+
+# Convert text to lowercase and remove punctuation
+text = text.lower()
+text = re.sub(r'[^\w\s]', '', text)
+
+# Split the text into words
+words = text.split()
+
+# delete stop words
+filtered_words = [word for word in words if word not in StopWords]
+
+# Combine filtered words with spaces
+new_text = " ".join(filtered_words)
+
+# Write the filtered text back to the file
+with open(path, "w") as file:
+    file.write(new_text)
+
+# Print the filtered text
+print("Filtered text:")
+print(new_text)
+
+# Count the frequency of each word in the filtered text
+word_counts = Counter(filtered_words)
+
+# Display the word frequency count to the console
+print("\nWord frequency count:")
+for word, count in word_counts.items():
+    print(f"{word}: {count}")
+
